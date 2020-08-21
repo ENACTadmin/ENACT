b0dd8a88bf133ee927dd38ecb648460bc7f9d350
SupremeEthan <43837579+SupremeEthan@users.noreply.github.com>
2020-08-21 13:48:18 -0400

    minor update on package.json

--- app.js
+++ app.js
@@ -127,7 +127,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //*******************************************
 //***********Login authorization*************
-
 let adminList = ["bbdhy96@gmail.com", "nicolezhang@brandeis.edu", "stimell@brandeis.edu", "djw@brandeis.edu", "epevide@brandeis.edu"]
 // here is where we check and assign user's status
 // this runs every time when a req is received
