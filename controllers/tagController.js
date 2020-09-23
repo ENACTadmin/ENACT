6d608f31ed3a107e030009133ba40e6c60e1d283
Huiyan0409 <nicolezhang@brandeis.edu>
2020-09-23 17:58:51 -0400

    tag change

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -8,14 +8,28 @@ exports.addTags = async (req, res, next) => {
             {
                 ownerId: req.user._id,
                 ownerStatus: req.user.status,
+                ownerName: req.user.userName,
                 info: req.body.addingTag,
-                status: 'underReview'
+                status: 'underReview',
+                reason: req.body.reason
             }
         )
         // await until the newCourse is saved properly
         await newTag.save()
         res.redirect('back')
     } catch (e) {
+        next(e)
+    }
+}
+
+exports.loadUnderReviewTags = async (req, res, next) => {
+    try {
+        res.locals.tagsInfo = await Tag.find({
+            status: 'underReview'
+        }).sort({'createdAt': -1})
+        next()
+    } catch (e) {
+        console.log("error: " + e)
         next(e)
     }
 }
