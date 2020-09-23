023c6c74dfac8d7296eb211e112eb932f37f236a
Huiyan0409 <nicolezhang@brandeis.edu>
2020-09-23 18:26:03 -0400

    enabled approve and deny tags

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -27,9 +27,37 @@ exports.loadUnderReviewTags = async (req, res, next) => {
         res.locals.tagsInfo = await Tag.find({
             status: 'underReview'
         }).sort({'createdAt': -1})
-        next()
+        res.render('./pages/approveTags')
     } catch (e) {
         console.log("error: " + e)
+        next(e)
+    }
+}
+
+exports.agreeTags = async (req, res, next) => {
+    try {
+        let tagsId = await req.body.checked
+        let tagsInfo = await Tag.find({_id: tagsId})
+        for (let i = 0; i < tagsInfo.length; i++) {
+            tagsInfo[i].status = 'approve'
+            tagsInfo[i].save()
+        }
+        res.redirect('back')
+    } catch (e) {
+        next(e)
+    }
+}
+
+exports.denyTags = async (req, res, next) => {
+    try {
+        let tagsId = await req.body.checked
+        let tagsInfo = await Tag.find({_id: tagsId})
+        for (let i = 0; i < tagsInfo.length; i++) {
+            tagsInfo[i].status = 'deny'
+            tagsInfo[i].save()
+        }
+        res.redirect('back')
+    } catch (e) {
         next(e)
     }
 }
