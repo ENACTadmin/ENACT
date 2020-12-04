5296fbacf9724bc3c43d16a7b9f26ce5ae7e7b6a
SupremeEthan <43837579+SupremeEthan@users.noreply.github.com>
2020-12-04 01:22:47 -0500

    major update, allow multiple authors...

--- public/js/authorship.js
+++ public/js/authorship.js
@@ -1,25 +1,19 @@
-(function() {
-    var counter = 0;
-    var btn = document.getElementById('addBtn');
-    var form = document.getElementById('form');
-    var addInput = function() {
-        counter++;
-        var span = document.createElement("span")
-            // <span class='input-group-text' style='width: 180px'>Name</span>
-        span.class = 'input-group-text'
-        span.style.width = '180px'
-        span.text = 'Name'
-        form.appendChild(span);
-
-        var input = document.createElement("input");
-        input.id = 'authorName-' + counter;
-        input.class = 'form-control'
-        input.type = 'text';
-        input.name = 'name';
-        input.placeholder = 'Input number ' + counter;
-        form.appendChild(input);
-    };
-    btn.addEventListener('click', function() {
-        addInput();
-    }.bind(this));
-})();
+$(document).ready(function () {
+    $("#addButton").click(function () {
+        var id = ($('.form-horizontal .input-group-prepend').length + 1).toString();
+        $('.form-horizontal').append('<br class="removeMe"><div class="input-group-prepend">' +
+            '<span class="input-group-text" style="width: 180px">Name</span>' +
+            '<input class="form-control" type="text" name="authorName" placeholder="Name ' + id + '" style="margin: 0 20px 0 0">' +
+            '<span class="input-group-text" style="width: 180px">Email</span>' +
+            '<input class="form-control" type="text" name="authorEmail" placeholder="Email ' + id + '" style="margin: 0 20px 0 0">' +
+            '</div>');
+    });
+    $("#removeButton").click(function () {
+        if ($('.form-horizontal .input-group-prepend').length == 1) {
+            alert("No more text-box to remove");
+            return false;
+        }
+        $(".form-horizontal .input-group-prepend:last").remove();
+        $(".form-horizontal .removeMe:last").remove();
+    });
+});
