97bfb54ec5db9b5e8298f3a17c7d41f13ee88c6c
SupremeEthan <43837579+SupremeEthan@users.noreply.github.com>
2021-02-09 15:01:09 -0500

    like button ajax

--- public/js/editModalAjax.js
+++ public/js/editModalAjax.js
@@ -1,36 +1,114 @@
-
 $(document).ready(function () {
-    "use strict"
-    let resource = null
-    // page is ready
-    $.ajax({
-        type: 'GET',
-        url: '/profiles/',
-        async: false,
-        dataType: 'json',
-        success: function (data) {
-            resource = data
-        }
+    // Delete modal
+    $('#deleteModal').hide()
+    $(".deleteBtn").click(function () {
+        let resource = null
+        $.ajax({
+            type: 'GET',
+            url: '/resource/' + this.id,
+            async: false,
+            dataType: 'json',
+            success: function (data) {
+                resource = data
+                // change form action attribute
+                $('#deleteForm').attr('action', '/resource/remove/' + resource._id)
+                $('#deleteModal').show()
+            }
+        });
     });
 
-    for(var profile in profiles){
-        profiles[profile].label = profiles[profile].userName;
-    }
-
-    var input = document.getElementById("profiles");
-    var ownerId = document.getElementById("ownerId");
-
-    autocomplete({
-        input: input,
-        fetch: function (text, update) {
-            text = text.toLowerCase();
-            // you can also use AJAX requests instead of preloaded data
-            var suggestions = profiles.filter(n => (n.label !== undefined && n.label.toLowerCase().startsWith(text)))
-            update(suggestions);
-        },
-        onSelect: function (item) {
-            input.value = item.userName;
-            ownerId.value = item._id;
+    // Edit modal
+    $('#editModal').hide()
+    $(".editBtn").click(function () {
+        let resource = null
+        $.ajax({
+            type: 'GET',
+            url: '/resource/' + this.id,
+            async: false,
+            dataType: 'json',
+            success: function (data) {
+                resource = data
+                // console.log("Resource: ", resource)
+                console.log(resource.name)
+                $('#ownerNames').text(resource.ownerName)
+                $('#resourceName').val(resource.name)
+                $('#description').text(resource.description)
+                $('#fileURL').val(resource.uri)
+                $('#institution').val(resource.institution)
+                $('#yearOfCreation').val(resource.yearOfCreation)
+                let contentType = resource.contentType
+                // console.log("Content type: ", resource.contentType)
+                $('#contentTypeSelect' + ' option').each(function () {
+                    if ($(this).val() === contentType) {
+                        $(this).prop('selected', true)
+                    }
+                })
+                let mediaType = resource.mediaType
+                console.log("Content type: ", resource.mediaType)
+                $('#mediaTypeSelect' + ' option').each(function () {
+                    if ($(this).val() === mediaType) {
+                        $(this).prop('selected', true)
+                    }
+                })
+                let state = resource.state
+                $('#stateSelect' + ' option').each(function () {
+                    if ($(this).val() === state) {
+                        $(this).prop('selected', true)
+                    }
+                })
+                let status = resource.status
+                $('#statusSelect' + ' option').each(function () {
+                    if ($(this).val() === status) {
+                        $(this).prop('selected', true)
+                    }
+                })
+                let tags = resource.tags.toString()
+                let tagsArray = tags.split(',')
+                $(':checkbox').each(function () {
+                    if (tagsArray.includes($(this).val())) {
+                        $(this).prop('checked', true)
+                    }
+                })
+                let checkboxes = $(':checkbox')
+                let string = checkboxes.filter(':checked').map(function () {
+                    return this.value;
+                }).get().join(',');
+                $('#tagsToReturn').val(string);
+                checkboxes.on('change', function () {
+                    let string = checkboxes.filter(':checked').map(function () {
+                        return this.value;
+                    }).get().join(',');
+                    $('#tagsToReturn').val(string);
+                })
+                // change form action attribute
+                $('#editForm').attr('action', '/resource/update/' + resource._id)
+                $('#editModal').show()
+            }
+        });
+        // display correct tags for different content types
+        let text = $('#contentTypeSelect option:selected').text();
+        if (text === 'Assignment Guidelines & Rubrics' || text === 'Course Reading' || text === 'ENACT Faculty Research' || text === 'Course Planning' || text === 'Online Teaching Resources' || text === 'Outlets for Publication' || text === 'Rubrics for grading' || text === 'State Legislative Website Resources') {
+            $(".toShow").hide()
+            $(".toShow2").hide()
+        } else if (text === 'Syllabi') {
+            $(".toShow2").show()
+            $(".toShow").hide()
+        } else {
+            $(".toShow").show()
+            $(".toShow2").hide()
         }
-    });
+        $("#contentTypeSelect").on("change", function () {
+            text = $('#contentTypeSelect option:selected').text();
+            if (text === 'Assignment Guidelines & Rubrics' || text === 'Course Reading' || text === 'ENACT Faculty Research' || text === 'Course Planning' || text === 'Online Teaching Resources' || text === 'Outlets for Publication' || text === 'Rubrics for grading' || text === 'State Legislative Website Resources') {
+                $(".toShow").hide()
+                $(".toShow2").hide()
+            } else if (text === 'Syllabi') {
+                $(".toShow2").show()
+                $(".toShow").hide()
+            } else {
+                $(".toShow").show()
+                $(".toShow2").hide()
+            }
+        })
+    })
 })
