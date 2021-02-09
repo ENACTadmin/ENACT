265df59d82b8b0fa9808361de1cc15985b27789d
SupremeEthan <43837579+SupremeEthan@users.noreply.github.com>
2021-02-09 16:26:47 -0500

    like

---
+++ public/js/likeAjax.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    // Delete modal
+    $(".focusMe").click(function () {
+        let id = this.id
+        // alert($('#' + id).html().trim() === $('#findMe').html("&#9825;").text())
+        if ($('#' + id).html().trim() === $('#findMe').html("&#9825;").text()) {
+            $.ajax({
+                type: 'POST',
+                url: '/resource/starAlt/' + this.id,
+                async: true,
+                dataType: 'text',
+                success: function () {
+                    $('#' + id).html('&#9829;');
+                }
+            })
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: '/resource/unstarAlt/' + this.id,
+                async: true,
+                dataType: 'text',
+                success: function () {
+                    $('#' + id).html('&#9825;');
+                }
+            })
+        }
+    })
+})
