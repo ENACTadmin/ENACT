1c73160c5439cc3989143d1f9c530003c2d72cc0
SupremeEthan <43837579+SupremeEthan@users.noreply.github.com>
2021-02-18 13:18:37 -0500

    email fix

--- app.js
+++ app.js
@@ -32,8 +32,7 @@ const utils = require('./controllers/utils');
 //***********Database connection*************
 
 // const MONGODB_URI = 'mongodb://localhost/ENACT';
-// const MONGODB_URI = process.env.MONGODB_URI_IND || 'mongodb://localhost/ENACT';
-const MONGODB_URI = 'mongodb+srv://heroku_s59qt61k:suo0sir3rh8b104b38574ju3dm@cluster-s59qt61k.xy6rv.mongodb.net/heroku_s59qt61k?retryWrites=true&w=majority' || 'mongodb://localhost/ENACT';
+const MONGODB_URI = process.env.MONGODB_URI_IND || 'mongodb://localhost/ENACT';
 const mongoose = require('mongoose');
 
 // Makes connection asynchronously.  Mongoose will queue up database
