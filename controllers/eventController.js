b19760e25f8741858574305de5b7784e88dbe740
naimulh247 <naimulh247@gmail.com>
2022-09-25 22:51:25 -0400

    revert test

---
+++ controllers/eventController.js
@@ -0,0 +1,183 @@
+'use strict';
+const Event = require('../models/Event');
+const User = require('../models/User');
+const sgMail = require('@sendgrid/mail');
+
+
+exports.saveEvent = async (req, res, next) => {
+    try {
+        let timezoneOffset = req.body.TZ
+        timezoneOffset = parseInt(timezoneOffset) - new Date().getTimezoneOffset()
+        let startDate = new Date(req.body.start).getTime() + parseInt(timezoneOffset) * 60000
+        let endDate = new Date(req.body.end).getTime() + parseInt(timezoneOffset) * 60000
+        let newEvent = new Event({
+            ownerId: req.user._id,
+            title: req.body.title,
+            start: startDate,
+            end: endDate,
+            uri: req.body.uri,
+            description: req.body.description,
+            visibility: req.body.visibility
+        })
+        // send event emails
+        let faculties = await User.find({status: {$in: ["admin", "faculty"]}})
+        for (let idx in faculties) {
+            let email = await faculties[idx].workEmail || faculties[idx].googleemail
+            if (email && email !== 'shekenne@iupui.edu') {
+                let eventName = newEvent.title
+                let eventTime = dayLightTime(newEvent.start)
+                let eventDescription = newEvent.description
+                let visibility = newEvent.visibility
+                let url = 'https://www.enactnetwork.org/login'
+                await sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+                const msg = {
+                    to: email,
+                    from: 'enact@brandeis.edu',
+                    subject: 'ENACT Digital Platform: a new ENACT event has been created',
+                    text: 'ENACT Digital Platform: a new ENACT event has been created',
+                    html: 'Dear ENACT Faculty Fellow,' +
+                        '<br><br>' + 'You may want to know about a new event: ' + '<br>' +
+                        '<b>' + eventName + '</b>' +
+                        '<br><br>' + 'Here is the event Description:' +
+                        '<br><br>' + eventDescription +
+                        '<br><br>' + 'Event will start at ' + '<b>' + eventTime.toLocaleString() + ' (in US/Canada Eastern Time)</b>' +
+                        '<br><br>' + 'Event visibility is: ' + '<b>' + visibility + '</b>' +
+                        '<br>Please click <a href=' + url + '>' + 'here' + '</a>' + ' to login, and more details can be viewed in ' + '<b>' + 'Events and Courses. ' + '</b>' +
+                        '<br><br><br>' + 'ENACT Support Team'
+                };
+                try {
+                    await sgMail.send(msg);
+                } catch (e) {
+                    console.log("SENDGRID EXCEPTION: ", e)
+                }
+            }
+        }
+        await newEvent.save()
+        res.redirect('back')
+    } catch (e) {
+        next(e)
+    }
+}
+
+exports.editEvent = async (req, res, next) => {
+    try {
+        let timezoneOffset = req.body.TZ
+        timezoneOffset = parseInt(timezoneOffset) - new Date().getTimezoneOffset()
+        let startDate = new Date(req.body.start).getTime() + parseInt(timezoneOffset) * 60000
+        let endDate = new Date(req.body.end).getTime() + parseInt(timezoneOffset) * 60000
+        let eventToEdit = await Event.findOne({_id: req.params.eventId})
+        eventToEdit.title = req.body.title
+        eventToEdit.start = startDate
+        eventToEdit.end = endDate
+        eventToEdit.uri = req.body.uri
+        eventToEdit.description = req.body.description
+        eventToEdit.visibility = req.body.visibility
+        await eventToEdit.save()
+        res.redirect('back')
+    } catch (e) {
+        next(e)
+    }
+}
+
+
+exports.deleteEvent = async (req, res, next) => {
+    try {
+        let eventId = req.params.eventId
+        await Event.deleteOne({_id: eventId})
+        res.redirect('/events')
+    } catch (e) {
+        next(e)
+    }
+}
+
+exports.updateImageURL = async (req, res, next) => {
+    let eventToUpdate = await Event.findOne({_id: req.params.eventId})
+    try {
+        eventToUpdate.imageURL = req.body.imageURL;
+        await eventToUpdate.save()
+        res.redirect('back')
+    } catch (e) {
+        next(e)
+    }
+}
+
+exports.sendEventEmail = async (req, res) => {
+    let eventId = req.params.id
+    let currEvent = await Event.findOne({_id: eventId})
+    let eventName = currEvent.title
+    let eventTime = dayLightTime(currEvent.start)
+    let eventDescription = currEvent.description
+    let visibility = currEvent.visibility
+    let faculties = await User.find({status: {$in: ["faculty", "admin"]}})
+    for (let faculty in faculties) {
+        let email = faculties[faculty].workEmail || faculties[faculty].googleemail
+        if (email && email !== 'shekenne@iupui.edu') {
+            let url = 'https://www.enactnetwork.org/login'
+            const sgMail = require('@sendgrid/mail');
+            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+            const msg = {
+                to: email,
+                from: 'enact@brandeis.edu',
+                subject: 'ENACT Digital Platform: a new ENACT event has been created',
+                text: 'ENACT Digital Platform: a new ENACT event has been created',
+                html: 'Dear ENACT Faculty Fellow,' +
+                    '<br><br>' + 'You may want to know about a new event: ' + '<br>' +
+                    '<b>' + eventName + '</b>' +
+                    '<br><br>' + 'Here is the event Description:' +
+                    '<br><br>' + eventDescription +
+                    '<br><br>' + 'Event will start at ' + '<b>' + eventTime.toLocaleString() + ' (in US/Canada Eastern Time)</b>' +
+                    '<br><br>' + 'Event visibility is: ' + '<b>' + visibility + '</b>' +
+                    '<br>Please click <a href=' + url + '>' + 'here' + '</a>' + ' to login, and more details can be viewed in ' + '<b>' + 'Events and Courses. ' + '</b>' +
+                    '<br><br><br>' + 'ENACT Support Team'
+            };
+            try {
+                await sgMail.send(msg);
+            } catch (e) {
+                console.log("SENDGRID EXCEPTION: ", e)
+            }
+        }
+    }
+    res.redirect('back')
+}
+
+exports.loadEvents = async (req, res, next) => {
+    let eventsInfo;
+    // if (res.locals.loggedIn) {
+    eventsInfo = await Event.find({}).sort({start: -1}).limit(3)
+    // } else {
+    //     eventsInfo = await Event.find({visibility: 'public'}).sort({start: -1}).limit(3)
+    // }
+    res.locals.eventsInfo = eventsInfo
+    next()
+}
+
+// helper functions to adjust time to EST considering daylight savings
+const dayLightTime = (inputTime) => {
+    // convert to msec since Jan 1 1970
+    const localTime = inputTime.getTime()
+    // obtain local UTC offset and convert to msec
+    const localOffset = inputTime.getTimezoneOffset() * 60 * 1000
+    // obtain UTC time in msec
+    const utcTime = localTime + localOffset
+    // obtain and add destination's UTC time offset
+    const estOffset = getEstOffset()
+    const EST = utcTime + (60 * 60 * 1000 * estOffset)
+    return new Date(EST)
+}
+
+const getEstOffset = () => {
+    const stdTimezoneOffset = () => {
+        var jan = new Date(0, 1)
+        var jul = new Date(6, 1)
+        return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset())
+    }
+    var today = new Date()
+    const isDstObserved = () => {
+        return today.getTimezoneOffset() < stdTimezoneOffset()
+    }
+    if (isDstObserved(today)) {
+        return -4
+    } else {
+        return -5
+    }
+}
