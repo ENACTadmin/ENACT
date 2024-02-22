40d79384ae85255afb82ad8da96d47c11cf1fa9a
mnamadina <madina@brandeis.edu>
2024-02-22 18:09:38 -0500

    listing current courses on the schedule page

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.set("view engine", "ejs");
 // app.use(logger("dev"));
 // app.use(logger("combined"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
